91ea1a931eb78ebaafbbd4c348a9f165e0f2c912
TyronicTrader <82331418+TyronicTrader@users.noreply.github.com>
2024-09-03 18:13:41 +0900

    compiz.sh
    cleanup

--- compiz.sh
+++ compiz.sh
@@ -14,10 +14,6 @@ sudo apt install -y cifs-utils smbclient
 # install terminal filemanager (ranger requires xorg but nnn does not so putting it here first)
 sudo apt install -y nnn
 
-# Base install files for base setup or development
-#sudo apt install -y dkms linux-headers-$(uname -r) build-essentials
-sudo apt install -y curl
-
 # Fonts and icons for now
 sudo apt install -y fonts-recommended fonts-ubuntu fonts-font-awesome fonts-terminus papirus-icon-theme fonts-noto fonts-anonymous-pro
 
@@ -28,7 +24,6 @@ sudo apt install -y xorg xbindkeys xinput xclip xarchiver gvfs-backends ranger
 # Base GUI
 sudo apt install -y lxde-core compiz compiz-bcop compiz-core compiz-dev compiz-gnome compiz-plugins compiz-plugins-default compiz-plugins-extra compiz-plugins-main compizconfig-settings-manager
 
-
 # Sound packages so we can hear vids in browser and also for Android Studio (need pulseaudio for it instead of pipewire)
 sudo apt install -y pulseaudio alsa-utils pavucontrol volumeicon-alsa pamixer
 
@@ -38,15 +33,16 @@ sudo apt install -y lxappearance breeze-gtk-theme breeze-icon-theme
 # Base utils
 sudo apt install -y timeshift galculator p7zip-rar
 sudo apt install -y thunar thunar-archive-plugin thunar-data thunar-font-manager thunar-media-tags-plugin thunar-vcs-plugin thunar-volman
-sudo apt install -y geany
-sudo apt install -y geany-plugin-addons geany-plugin-git-changebar geany-plugin-overview geany-plugin-spellcheck geany-plugin-treebrowser geany-plugin-vimode
-sudo apt install -y geany-plugins \ # all plugins
+sudo apt install -y geany geany-plugins geany-plugin-addons geany-plugin-git-changebar geany-plugin-overview geany-plugin-spellcheck geany-plugin-treebrowser geany-plugin-vimode
+
+# geany color-themes
 git clone https://github.com/TyronicTrader/geany-themes.git ~/Downloads
 mkdir $HOME/.config/geany/
 mkdir $HOME/.config/geany/colorschemes/
 mv $HOME/Downloads/geany-themes/colorschemes/* $HOME/.config/geany/colorschemes/
 
 # install Brave Browser
+sudo apt install -y curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
