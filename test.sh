e39416da5f2a5da8e14014cb8e5b1c9f0716b743
TyronicTrader <82331418+TyronicTrader@users.noreply.github.com>
2024-09-04 03:04:06 +0900

    Delete test.sh

--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env/bash
-
-sudo apt install -y clang-14 clang-14-doc clang-14-examples clang-tools-14 ccls clang-format-14 clang-tidy-14 clangd-14 libclang-14-dev libclang-common-14-dev libclang1-14 liblldb-14-dev libopencl-clang-14-dev lldb-14 libclang-cpp14 libclang-cpp14-dev libclang-rt-14-dev llvm-14 llvm-14-doc llvm-14-dev llvm-14-examples llvm-14-linker-tools llvm-14-runtime llvm-14-tools 
