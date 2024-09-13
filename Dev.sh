d32e5f4f56aa6202e77897a899cf12c5c41310b0
TyronicTrader <82331418+TyronicTrader@users.noreply.github.com>
2024-09-13 18:13:08 +0900

    Update Dev.sh

--- Dev.sh
+++ Dev.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
+sudo apt update
+sudo apt upgrade -y
+
 ####    INSTALL CLANG LLVM
 sudo apt install -y clang-14 clang-14-doc clang-14-examples clang-tools-14 clang-format-14 clang-tidy-14 clangd-14 libclang-14-dev libclang-common-14-dev libclang1-14 liblldb-14-dev libopencl-clang-14-dev lldb-14 libclang-cpp14 libclang-cpp14-dev libclang-rt-14-dev
 sudo apt install -y llvm-14 llvm-14-doc llvm-14-dev llvm-14-examples llvm-14-linker-tools llvm-14-runtime llvm-14-tools lld-14 lldb-14 libc++-14-dev libc++1-14 libc++abi-14-dev libc++abi1-14 liblld-14 liblld-14-dev liblldb-14 liblldb-14-dev libllvm14 libomp-14-dev libomp-14-doc libomp5-14
