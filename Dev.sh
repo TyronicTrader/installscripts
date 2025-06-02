4c711d489e3915a464a84e8be302afcd7c40a8dc
TyronicTrader <82331418+TyronicTrader@users.noreply.github.com>
2025-06-03 03:25:45 +0900

    Update
    added libstdc++-14-dev

--- Dev.sh
+++ Dev.sh
@@ -7,7 +7,7 @@ sudo apt upgrade -y
 sudo apt install -y clang-14 clang-14-doc clang-14-examples clang-tools-14 clang-format-14 clang-tidy-14 clangd-14 libclang-14-dev libclang-common-14-dev libclang1-14 liblldb-14-dev libopencl-clang-14-dev lldb-14 libclang-cpp14 libclang-cpp14-dev libclang-rt-14-dev
 sudo apt install -y llvm-14 llvm-14-doc llvm-14-dev llvm-14-examples llvm-14-linker-tools llvm-14-runtime llvm-14-tools lld-14 lldb-14 libc++-14-dev libc++1-14 libc++abi-14-dev libc++abi1-14 liblld-14 liblld-14-dev liblldb-14 liblldb-14-dev libllvm14 libomp-14-dev libomp-14-doc libomp5-14
 sudo atp install -y clang ccls clang-format clang-tidy clang-tools clangd clazy libclang-cpp-dev libclang-dev libclang1
-sudo atp install -y libc++-dev libc++1 libc++abi-dev libc++abi1 liblld-dev liblldb-dev libomp-dev libomp5 lld lldb llvm llvm-dev llvm-runtime
+sudo atp install -y libc++-dev libc++1 libc++abi-dev libc++abi1 liblld-dev liblldb-dev libomp-dev libomp5 lld lldb llvm llvm-dev llvm-runtime libstdc++-14-dev
 sudo apt install -y cmake cmake-data cmake-doc cmake-extras cmake-format ninja-build generate-ninja
 export PATH=$PATH:/usr/lib/llvm-14/bin
 echo -e 'export PATH=$PATH:/usr/lib/llvm-14/bin' | tee -a $HOME/.bashrc
